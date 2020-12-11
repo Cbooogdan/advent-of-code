21726c7435421f6937b88895ea857dd653032458
Bogdan Corolea <bogdan@graffino.com>
2020-12-11 23:57:13 +0200

    Resolve puzzle 2 day 11 WIP

--- day-10/index.js
+++ day-10/index.js
@@ -5,16 +5,26 @@
 
 let input = global.loadInput().lines();
 // let input = document.querySelector('pre').textContent.trim().split('\n');
+input = input.map((jolt => parseInt(jolt))).sort( (a,b ) => a - b );
+input = [0, ...input, input[input.length-1] + 3];
 
 // Puzzle 1
-input = input.map((jolt => parseInt(jolt))).sort( (a,b ) => a - b );
-input = [0, ...input];
-differencesCounter = [0, 0, 0]; // 1st for diff of 1 and so on
+let differencesCounter = [0, 0, 0]; // 1st for diff of 1 and so on
 for (let index = 1; index < input.length; index++ ) {
   let diff = input[index] - input[index - 1];
   differencesCounter[diff-1] += 1;
 };
-differencesCounter[2] += 1;
 console.log(differencesCounter[0] * differencesCounter[2]);
 
 // Puzzle 2
+let setOfArrangements = new Set();
+setOfArrangements.add(input);
+do {
+  setOfArrangements.forEach(function(value) {
+    console.log('1');
+    return;
+  });
+  return;
+  console.log('2');
+} while (false);
+console.log(differencesCounter[0] * differencesCounter[2]);
