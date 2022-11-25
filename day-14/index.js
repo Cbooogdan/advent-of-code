116e0d5868bff21ade69c65ed078de923ed09b51
Bogdan Corolea <bcorolea@softia.ro>
2022-11-25 22:51:17 +0200

    Replace array with maps

--- day-14/index.js
+++ day-14/index.js
@@ -9,7 +9,7 @@ let input = global.loadInput().lines();
 
 // Puzzle 1
 let mask = '';
-let memory = [];
+let memory = new Map();
 
 input.forEach(line => {
   line = line.split(' = ');
@@ -23,23 +23,23 @@ input.forEach(line => {
   value = parseInt(value).toString(2);
 
   const memoryAddress = firstSequence.substr(4, firstSequence.length - 5);
-  memory[memoryAddress] = '';
+  memory.set(memoryAddress, '');
 
   for(let index = 1; index <= 36; index++) {
     const charMask = mask.substr(-index, 1);
     let charValue = value.substr(-index, 1);
     charValue = index > value.length ? '0' : charValue;
 
-    const valueInBinary = (charMask !== 'X' ? charMask : charValue) + memory[memoryAddress];
-    memory[memoryAddress] = parseInt(valueInBinary, 2);
+    const valueInBinary = (charMask !== 'X' ? charMask : charValue) + memory.get(memoryAddress);
+    memory.set(memoryAddress, parseInt(valueInBinary, 2));
   }
 });
 
-const resultPuzzle1 = memory.reduce((prevElement, nextElement) => prevElement + nextElement);
+const resultPuzzle1 = Array.from(memory.values()).reduce((prevElement, nextElement) => prevElement + nextElement);
 
 // Puzzle 2
 mask = '';
-memory = [];
+memory = new Map();
 
 input.forEach((line, index) => {
   line = line.split(' = ');
@@ -68,8 +68,6 @@ input.forEach((line, index) => {
 
   while (memoryAddressesBinary.length < numberOfCombinations) {
 
-    // console.log(index, memoryAddressesBinary.length, numberOfCombinations);
-
     const originalValues = [...memoryAddressesBinary];
     memoryAddressesBinary = [];
 
@@ -80,17 +78,12 @@ input.forEach((line, index) => {
   }
 
   memoryAddressesBinary.forEach(address => {
-    memory[parseInt(address, 2)] = parseInt(value);
+    memory.set(parseInt(address, 2), parseInt(value));
   });
 });
 
-const resultPuzzle2 = memory.reduce((prevElement, nextElement, index) => {
-  console.log(prevElement + nextElement, index);
-  return prevElement + nextElement;
-});
+const resultPuzzle2 = Array.from(memory.values()).reduce((prevElement, nextElement) => prevElement + nextElement);
 
-console.log('FINISH!!!!');
-console.log(memory.length);
 console.log(resultPuzzle2);
 
 
